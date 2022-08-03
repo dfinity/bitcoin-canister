5c7f64e14691e2dcbaf6aca558d9330ce8a1b451
Islam El-Ashi <ielashi@users.noreply.github.com>
2022-08-03 16:40:50 +0200

    Uncomment `unstableblocks` and `blocktree` modules (#2)
    Uncomments the blocktree and unstableblocks modules to include them in our compilation.
    
    Additionally, I added a github workflow that runs the cargo tests + a small shell check.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 TARGET="wasm32-unknown-unknown"
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-pushd $SCRIPT_DIR
+pushd "$SCRIPT_DIR"
 
 # NOTE: On macOS a specific version of llvm-ar and clang need to be set here.
 # Otherwise the wasm compilation of rust-secp256k1 will fail.
