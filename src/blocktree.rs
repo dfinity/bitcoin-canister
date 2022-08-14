f77fffd9c39967117b087944b0c9573db3fb5d58
Islam El-Ashi <ielashi@users.noreply.github.com>
2022-08-14 18:23:15 +0200

    Enforce clippy checks (#9)

--- src/blocktree.rs
+++ src/blocktree.rs
@@ -43,7 +43,7 @@ impl<'a> BlockChain<'a> {
         self.first
     }
 
-    pub fn tip(&self) -> &'a Block {
+    /*pub fn tip(&self) -> &'a Block {
         match self.successors.last() {
             None => {
                 // The chain consists of only one block, and that is the tip.
@@ -51,7 +51,7 @@ impl<'a> BlockChain<'a> {
             }
             Some(tip) => tip,
         }
-    }
+    }*/
 
     /// Consumes this `BlockChain` and returns the entire chain of blocks.
     pub fn into_chain(self) -> Vec<&'a Block> {
