994b273fcaa1c9bb703969f03398cff0c1b6950f
Islam El-Ashi <islam.elashi@dfinity.org>
2022-08-17 15:00:16 +0200

    EXC-1187: Add pre_upgrade / post_upgrade hooks

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ mod utxoset;
 
 use crate::{state::State, types::InitPayload};
 use bitcoin::blockdata::constants::genesis_block;
+use stable_structures::Memory;
 use std::cell::RefCell;
 
 thread_local! {
@@ -23,6 +24,13 @@ thread_local! {
 /// Precondition: the state is already initialized.
 pub fn with_state<R>(f: impl FnOnce(&State) -> R) -> R {
     STATE.with(|cell| f(cell.borrow().as_ref().expect("state not initialized")))
+}
+
+/// A helper method to mutate the state.
+///
+/// Precondition: the state is already initialized.
+pub fn with_state_mut<R>(f: impl FnOnce(&mut State) -> R) -> R {
+    STATE.with(|cell| f(cell.borrow_mut().as_mut().expect("state not initialized")))
 }
 
 // A helper method to set the state.
@@ -50,11 +58,42 @@ pub fn init(payload: InitPayload) {
     ))
 }
 
+pub fn pre_upgrade() {
+    // Serialize the state.
+    let mut state_bytes = vec![];
+    with_state(|state| ciborium::ser::into_writer(state, &mut state_bytes))
+        .expect("failed to encode state");
+
+    // Write the length of the serialized bytes to memory, followed by the
+    // by the bytes themselves.
+    let len = state_bytes.len() as u32;
+    let memory = memory::get_upgrades_memory();
+    memory.write(0, &len.to_le_bytes());
+    memory.write(4, &state_bytes);
+}
+
+pub fn post_upgrade() {
+    let memory = memory::get_upgrades_memory();
+
+    // Read the length of the state bytes.
+    let mut state_len_bytes = [0; 4];
+    memory.read(0, &mut state_len_bytes);
+    let state_len = u32::from_le_bytes(state_len_bytes) as usize;
+
+    // Read the bytes
+    let mut state_bytes = vec![0; state_len];
+    memory.read(4, &mut state_bytes);
+
+    // Deserialize and set the state.
+    let state = ciborium::de::from_reader(&*state_bytes).expect("failed to decode state");
+    set_state(state);
+}
+
 #[cfg(test)]
 mod test {
-    use proptest::prelude::*;
     use super::*;
-    use crate::types::Network;
+    use crate::{test_utils::build_regtest_chain, types::Network};
+    use proptest::prelude::*;
 
     proptest! {
         #[test]
@@ -76,6 +115,45 @@ mod test {
                     *state == State::new(stability_threshold, network, genesis_block(network.into()))
                 );
             });
+        }
+    }
+
+    proptest! {
+        #![proptest_config(ProptestConfig::with_cases(1))]
+        #[test]
+        fn upgrade(
+            stability_threshold in 1..100u32,
+            num_blocks in 1..250u32,
+            num_transactions_in_block in 1..100u32,
+        ) {
+            let network = Network::Regtest;
+
+            init(InitPayload {
+                stability_threshold,
+                network,
+            });
+
+            let blocks = build_regtest_chain(num_blocks, num_transactions_in_block);
+
+            // Insert all the blocks. Note that we skip the genesis block, as that
+            // is already included as part of initializing the state.
+            for block in blocks[1..].iter() {
+                with_state_mut(|s| {
+                    crate::store::insert_block(s, block.clone()).unwrap();
+                });
+            }
+
+            // Run the preupgrade hook.
+            pre_upgrade();
+
+            // Take out the old state (which also clears the `STATE` singleton).
+            let old_state = STATE.with(|cell| cell.take().unwrap());
+
+            // Run the postupgrade hook.
+            post_upgrade();
+
+            // The new and old states should be equivalent.
+            with_state(|new_state| assert!(new_state == &old_state));
         }
     }
 }
--- src/memory.rs
+++ src/memory.rs
@@ -1,28 +1,55 @@
-//! The purpose of this module is to make the memory being used in unit tests and
-//! in production functionally equivalent.
-//!
-//! In production, a `DefaultMemoryImpl` resolves to the `Ic0StableMemory`,
-//! which is a thin wrapper around the stable memory API.
-//!
-//! In unit tests, a `DefaultMemoryImpl` resolves to the `VectorMemory`, which
-//! simulates a stable memory using a `Vec`.
-//!
-//! There's a subtle difference between the two environments: the stable memory
-//! in production is a singleton, while the vector memories created in unit
-//! tests are not. This difference can cause unit tests to fail that would
-//! otherwise succeed in production and vice versa.
-//!
-//! This code removes this distinction and makes the two environments
-//! consistent, making the memory being used a singleton in both production and
-//! in unit tests.
-use stable_structures::DefaultMemoryImpl;
+use stable_structures::{DefaultMemoryImpl, RestrictedMemory};
+use std::ops::Range;
 
+// The purpose of this singleton is to make the memory being used in unit tests
+// and in production functionally equivalent.
+//
+// In production, a `DefaultMemoryImpl` resolves to the `Ic0StableMemory`,
+// which is a thin wrapper around the stable memory API.
+//
+// In unit tests, a `DefaultMemoryImpl` resolves to the `VectorMemory`, which
+// simulates a stable memory using a `Vec`.
+//
+// There's a subtle difference between the two environments: the stable memory
+// in production is a singleton, while the vector memories created in unit
+// tests are not. This difference can cause unit tests to fail that would
+// otherwise succeed in production and vice versa.
+//
+// This singleton removes this distinction and makes the two environments
+// consistent, making the memory being used a singleton in both production and
+// in unit tests.
 thread_local! {
     static MEMORY: DefaultMemoryImpl = DefaultMemoryImpl::default();
 }
 
-/// Returns an instance of the memory.
-/// The memory is a singleton that is cheaply cloned.
-pub fn get() -> DefaultMemoryImpl {
+// A memory used for storing bits of the state during updates..
+const UPGRADES_MEMORY: Range<u64> = 0..1_000;
+
+// Memories for stable structures.
+// NOTE: The sizes specified below are for testing purposes and are
+//       insufficient for production.
+const ADDRESS_OUTPOINTS: Range<u64> = 1_000..2_000;
+const UTXOS_SMALL: Range<u64> = 2_000..3_000;
+const UTXOS_MEDIUM: Range<u64> = 4_000..5_000;
+
+pub fn get_upgrades_memory() -> RestrictedMemory<DefaultMemoryImpl> {
+    RestrictedMemory::new(get_memory(), UPGRADES_MEMORY)
+}
+
+pub fn get_address_outpoints_memory() -> RestrictedMemory<DefaultMemoryImpl> {
+    RestrictedMemory::new(get_memory(), ADDRESS_OUTPOINTS)
+}
+
+pub fn get_utxos_small_memory() -> RestrictedMemory<DefaultMemoryImpl> {
+    RestrictedMemory::new(get_memory(), UTXOS_SMALL)
+}
+
+pub fn get_utxos_medium_memory() -> RestrictedMemory<DefaultMemoryImpl> {
+    RestrictedMemory::new(get_memory(), UTXOS_MEDIUM)
+}
+
+// Returns an instance of the memory.
+// The memory is a singleton that is cheaply cloned.
+fn get_memory() -> DefaultMemoryImpl {
     MEMORY.with(|m| m.clone())
 }
