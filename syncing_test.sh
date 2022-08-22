2ba884e19b39c31df0ace7812eb74038e20f51f5
Islam El-Ashi <islam.elashi@dfinity.org>
2022-08-22 12:01:30 +0200

    .

---
+++ syncing_test.sh
@@ -0,0 +1,20 @@
+rm -rf .dfx
+dfx start --background
+dfx deploy --no-wallet management_mock
+
+# Deploy the bitcoin canister
+dfx deploy --no-wallet bitcoin --argument "(record { stability_threshold = 2; network = variant { regtest }; management_canister = principal \"$(dfx canister id management_mock)\" })"
+
+sleep 5
+
+BALANCE=$(dfx canister call bitcoin get_balance '(record { address = "bcrt1qg4cvn305es3k8j69x06t9hf4v5yx4mxdaeazl8" })')
+
+dfx stop
+
+if [[ $BALANCE = "(5_000_000_000 : nat64)" ]]; then
+  echo "SUCCESS"
+  return 0
+else
+  echo "FAIL"
+  return 1
+fi
