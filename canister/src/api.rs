f7732c6821af22c53db76e102056cbaba35664bf
Islam El-Ashi <islam.elashi@dfinity.org>
2022-11-02 09:06:34 -0400

    feat: add ability to update stability threshold. (#83)
    We'll be slowly increasing the stability threshold for canisters so this
    will come in handy soon.

--- canister/src/api.rs
+++ canister/src/api.rs
@@ -1,6 +1,8 @@
 mod fee_percentiles;
 mod get_balance;
 mod get_utxos;
+mod set_config;
 pub use fee_percentiles::get_current_fee_percentiles;
 pub use get_balance::get_balance;
 pub use get_utxos::get_utxos;
+pub use set_config::set_config;
