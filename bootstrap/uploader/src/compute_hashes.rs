e8390b73f79bbbf74fb979c5200e8325abf2845c
Islam El-Ashi <islam.elashi@dfinity.org>
2022-11-23 14:56:37 +0100

    feat: [EXC-1286] Validate the bytes passed to the uploader canister.
    To ensure the correctness of the bytes uploaded via the `uploader`
    canister, the `uploader` canister is now initialized with a list of
    the sha2 hashes of each chunk that'll be uploaded. The `uploader` then
    verifies the hash of each chunk prior to writing it into stable memory.
    
    A script for compute the chunk hashes is also included.

---
+++ bootstrap/uploader/src/compute_hashes.rs
@@ -0,0 +1,35 @@
+//! A script for computing the hashes of the chunks of a file.
+//!
+//! The size of the chunk is hard-coded in `CHUNK_SIZE_IN_BYTES`.
+use clap::Parser;
+use std::{fs::File, io::Read, path::PathBuf};
+use uploader::*;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// The path of the file to compute its hashes.
+    #[clap(long, value_hint = clap::ValueHint::DirPath)]
+    file: PathBuf,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    // Read file in chunks, and print the sha256 hash of each of these chunks.
+    let mut file = File::open(args.file).expect("opening file must succeed");
+    let mut chunk = vec![0; CHUNK_SIZE_IN_BYTES as usize];
+    loop {
+        let bytes_read = file.read(&mut chunk).unwrap();
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        let hash = sha256::digest(&chunk[0..bytes_read]);
+        println!("{}", hash);
+
+        if bytes_read < CHUNK_SIZE_IN_BYTES as usize {
+            break;
+        }
+    }
+}
