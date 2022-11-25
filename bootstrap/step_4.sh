4898ddd7cd4ba0506a15b5b577c33b3578ec2f56
Islam El-Ashi <islam.elashi@dfinity.org>
2022-11-25 08:41:05 +0100

    .

--- bootstrap/step_4.sh
+++ bootstrap/step_4.sh
@@ -10,11 +10,12 @@ tail -n +2 utxodump.csv > utxodump.csv.tmp && mv utxodump.csv.tmp utxodump.csv
 echo "Sorting the file..."
 sort -n -o utxodump.csv utxodump.csv
 
+echo "Computing sorted UTXO checksum..."
+sha256sum utxodump.csv
+
 echo "Shuffling the file..."
 # Shuffling helps reduce the memory footprint of the stable btreemaps in the canister.
-RAND_SEED=$(mktemp)
-echo "1" > "$RAND_SEED"
-sort -R utxodump.csv --random-source="$RAND_SEED" > utxodump.csv.tmp && mv utxodump.csv.tmp utxodump.csv
+sort -R utxodump.csv --random-source=/dev/zero > utxodump.csv.tmp && mv utxodump.csv.tmp utxodump.csv
 
-echo "Computing UTXO dump checksum..."
+echo "Computing shuffled UTXO checksum..."
 sha256sum utxodump.csv
