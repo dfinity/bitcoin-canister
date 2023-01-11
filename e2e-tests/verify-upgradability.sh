bd95c008b34a619cbc9ebd7bd6fc37c4d83a5e07
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-01-11 13:30:19 +0000

    .

--- e2e-tests/verify-upgradability.sh
+++ e2e-tests/verify-upgradability.sh
@@ -8,6 +8,8 @@ trap "dfx stop & rm newest_release.wasm" EXIT SIGINT
 
 # Get newest release download url
 NEWEST_RELEASE="$(curl -s https://api.github.com/repos/dfinity/bitcoin-canister/releases/latest | grep "browser_download_url" | awk '{ print $2 }' | sed 's/,$//' | sed 's/"//g')"
+MANAGEMENT_CANISTER="aaaaa-aa"
+
 
 # Get newest release
 wget -O newest_release.wasm.gz "${NEWEST_RELEASE}"
@@ -16,13 +18,11 @@ gunzip newest_release.wasm.gz
 
 dfx start --background --clean
 
-dfx deploy --no-wallet e2e-scenario-3
-
 # Deploy newest release
 dfx deploy --no-wallet bitcoin-release --argument "(record { 
  stability_threshold = 2;
  network = variant { regtest };
- blocks_source = principal \"$(dfx canister id e2e-scenario-3)\";
+ blocks_source = principal \"$(dfx canister id "${MANAGEMENT_CANISTER}")\";
  fees = record { 
     get_utxos_base = 0; 
     get_utxos_cycles_per_ten_instructions = 0; 
@@ -41,13 +41,13 @@ dfx canister stop bitcoin-release
 
 # replace from bitcoin-release to bitcoin in .dfx/local/canister_ids.json
 # hence, the upgraded canister has the same CanisteID 
-sed -i 's/bitcoin-release/bitcoin/' .dfx/local/canister_ids.json 
+sed -i 's/bitcoin-release/bitcoin/' ./.dfx/local/canister_ids.json 
 
 # Deploy upgraded canister
 dfx deploy --no-wallet bitcoin --argument "(record { 
  stability_threshold = 2;
  network = variant { regtest };
- blocks_source = principal \"$(dfx canister id e2e-scenario-3)\";
+ blocks_source = principal \"$(dfx canister id "${MANAGEMENT_CANISTER}")\";
  fees = record { 
     get_utxos_base = 0; 
     get_utxos_cycles_per_ten_instructions = 0; 
