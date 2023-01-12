7cbe4aada6880ae28603aff5d915e1872ebf9e30
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-01-12 08:57:27 +0000

    Refactor.

--- e2e-tests/verify-upgradability.sh
+++ e2e-tests/verify-upgradability.sh
@@ -7,14 +7,7 @@ trap "dfx stop & rm newest_release.wasm.gz" EXIT SIGINT
 # Get newest release download url
 NEWEST_RELEASE="$(curl -s https://api.github.com/repos/dfinity/bitcoin-canister/releases/latest | grep "browser_download_url" | awk '{ print $2 }' | sed 's/,$//' | sed 's/"//g')"
 MANAGEMENT_CANISTER="aaaaa-aa"
-
-# Get newest release
-wget -O newest_release.wasm.gz "${NEWEST_RELEASE}"
-
-dfx start --background --clean
-
-# Deploy newest release
-dfx deploy --no-wallet bitcoin-release --argument "(record { 
+ARGUMENT="(record { 
  stability_threshold = 2;
  network = variant { regtest };
  blocks_source = principal \"$(dfx canister id "${MANAGEMENT_CANISTER}")\";
@@ -31,6 +24,14 @@ dfx deploy --no-wallet bitcoin-release --argument "(record {
  syncing = variant { enabled }; 
  api_access = variant { enabled }
 })"
+
+# Get newest release
+wget -O newest_release.wasm.gz "${NEWEST_RELEASE}"
+
+dfx start --background --clean
+
+# Deploy newest release
+dfx deploy --no-wallet bitcoin-release --argument "${ARGUMENT}"
 
 dfx canister stop bitcoin-release
 
@@ -39,45 +40,12 @@ dfx canister stop bitcoin-release
 sed -i 's/bitcoin-release/bitcoin/' ./.dfx/local/canister_ids.json 
 
 # Deploy upgraded canister
-dfx deploy --no-wallet bitcoin --argument "(record { 
- stability_threshold = 2;
- network = variant { regtest };
- blocks_source = principal \"$(dfx canister id "${MANAGEMENT_CANISTER}")\";
- fees = record { 
-    get_utxos_base = 0; 
-    get_utxos_cycles_per_ten_instructions = 0; 
-    get_utxos_maximum = 0; get_balance = 0; 
-    get_balance_maximum = 0; 
-    get_current_fee_percentiles = 0; 
-    get_current_fee_percentiles_maximum = 0;  
-    send_transaction_base =0; 
-    send_transaction_per_byte = 0; 
- }; 
- syncing = variant { enabled }; 
- api_access = variant { enabled }
-})"
+dfx deploy --no-wallet bitcoin --argument "${ARGUMENT}"
 
 dfx canister start bitcoin
 dfx canister stop bitcoin
 
-dfx deploy --upgrade-unchanged bitcoin --argument "(record { 
- stability_threshold = 2;
- network = variant { regtest };
- blocks_source = principal \"$(dfx canister id "${MANAGEMENT_CANISTER}")\";
- fees = record { 
-    get_utxos_base = 0; 
-    get_utxos_cycles_per_ten_instructions = 0; 
-    get_utxos_maximum = 0; get_balance = 0; 
-    get_balance_maximum = 0; 
-    get_current_fee_percentiles = 0; 
-    get_current_fee_percentiles_maximum = 0;  
-    send_transaction_base =0; 
-    send_transaction_per_byte = 0; 
- }; 
- syncing = variant { enabled }; 
- api_access = variant { enabled }
-})"
-
+dfx deploy --upgrade-unchanged bitcoin --argument "${ARGUMENT}"
 dfx canister start bitcoin
 
 echo "SUCCESS"
