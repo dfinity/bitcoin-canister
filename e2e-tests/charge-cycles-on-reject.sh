66659b720731bb46415d3c3a197c8890fe1686e6
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-01-31 17:00:30 +0000

    .

---
+++ e2e-tests/charge-cycles-on-reject.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+set -Eexuo pipefail
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/utils.sh"
+
+# Run dfx stop if we run into errors.
+trap "dfx stop" EXIT SIGINT
+
+dfx start --background --clean
+
+# Deploy the canister that returns the blocks for harge-cycles-on-reject.
+dfx deploy e2e-scenario-3
+
+# Deploy the bitcoin canister, setting the blocks_source to be the source above.
+dfx deploy bitcoin --argument "(record {
+  stability_threshold = 2;
+  network = variant { regtest };
+  blocks_source = principal \"$(dfx canister id e2e-scenario-3)\";
+  syncing = variant { enabled };
+  fees = record {
+    get_utxos_base = 0;
+    get_utxos_cycles_per_ten_instructions = 0;
+    get_utxos_maximum = 0;
+    get_balance = 0;
+    get_balance_maximum = 0;
+    get_current_fee_percentiles = 0;
+    get_current_fee_percentiles_maximum = 0;
+    send_transaction_base = 500000000000;
+    send_transaction_per_byte = 2000000000;
+  };
+  api_access = variant { enabled }
+})"
+
+dfx wallet balance --precise
+
+dfx wallet authorize $(dfx canister id bitcoin)
+
+# Send transaction valid transaction
+TX_BYTES="blob \"\\00\\00\\00\\00\\00\\01\\00\\00\\00\\00\\00\\00\""
+dfx canister --wallet=$(dfx identity get-wallet) call --with-cycles 624000000000 bitcoin bitcoin_send_transaction "(record {
+  network = variant { regtest };
+  transaction = ${TX_BYTES}
+})"
+
+# Verify the transaction was sent.
+TX_BYTES_RECEIVED=$(dfx canister call e2e-scenario-3 get_last_transaction --query)
+if ! [[ $TX_BYTES_RECEIVED = "($TX_BYTES)" ]]; then
+  echo "FAIL"
+  exit 1
+fi
+
+BEFORE_SEND_TRANSACTION=$(dfx wallet balance --precise)
+
+# Send invalid transaction.
+set +e
+TX_BYTES="blob \"12341234789789\""
+SEND_TX_OUTPUT=$(dfx canister  --wallet=$(dfx identity get-wallet) call --with-cycles 624000000000 bitcoin bitcoin_send_transaction "(record {
+  network = variant { regtest };
+  transaction = ${TX_BYTES}
+})" 2>&1);
+set -e
+
+
+# Should reject.
+if [[ $SEND_TX_OUTPUT != *"Cannot decode transaction"* ]]; then
+  echo "FAIL"
+  exit 1
+fi
+
+AFTER_SEND_TRANSACTION=$(dfx wallet balance --precise)
+
+# Should charge cycles.
+if [[ $BEFORE_SEND_TRANSACTION = $AFTER_SEND_TRANSACTION ]]; then
+  echo "FAIL"
+  exit 1
+fi
+
+echo "SUCCESS"
