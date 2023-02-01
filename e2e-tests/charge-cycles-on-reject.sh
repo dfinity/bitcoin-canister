2f48e5be3f008884fa95678acc86bc53ac84d258
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-02-01 12:18:19 +0000

    Script refactoring.

--- e2e-tests/charge-cycles-on-reject.sh
+++ e2e-tests/charge-cycles-on-reject.sh
@@ -19,12 +19,12 @@ dfx deploy bitcoin --argument "(record {
   blocks_source = principal \"$(dfx canister id e2e-scenario-3)\";
   syncing = variant { enabled };
   fees = record {
-    get_utxos_base = 500000000000;
-    get_utxos_cycles_per_ten_instructions = 500000000000;
+    get_utxos_base = 5000000000;
+    get_utxos_cycles_per_ten_instructions = 500000000;
     get_utxos_maximum = 500000000000;
-    get_balance = 500000000000;
+    get_balance = 5000000000;
     get_balance_maximum = 500000000000;
-    get_current_fee_percentiles = 500000000000;
+    get_current_fee_percentiles = 5000000000;
     get_current_fee_percentiles_maximum = 500000000000;
     send_transaction_base = 500000000000;
     send_transaction_per_byte = 2000000000;
@@ -34,11 +34,11 @@ dfx deploy bitcoin --argument "(record {
 
 check_charging()
 {
-  WALLET=$1
-  METHOD=$2
-  RECORD=$3
-  EXPECTED=$4
+  METHOD=$1
+  RECORD=$2
+  EXPECTED=$3
 
+  WALLET="$(dfx identity get-wallet)"
   BEFORE_SEND_TRANSACTION=$(dfx wallet balance --precise)
 
   # Send invalid transaction.
@@ -62,36 +62,35 @@ check_charging()
   fi
 }
 
-WALLET="$(dfx identity get-wallet)"
+#test bitcoin_send_transaction
 TX_BYTES="blob \"12341234789789\""
 METHOD="bitcoin_send_transaction"
 RECORD="(record { network = variant { regtest }; transaction = ${TX_BYTES}})"
 EXPECTED="Cannot decode transaction"
-check_charging "${WALLET}" "${METHOD}" "${RECORD}" "${EXPECTED}"
+check_charging "${METHOD}" "${RECORD}" "${EXPECTED}"
 
+#test bitcoin_get_balance
 METHOD="bitcoin_get_balance"
 RECORD="(record { address = \"Bad address\"; network = variant { regtest } })"
 EXPECTED="MalformedAddress"
-check_charging "${WALLET}" "${METHOD}" "${RECORD}" "${EXPECTED}"
+check_charging "${METHOD}" "${RECORD}" "${EXPECTED}"
 
-METHOD="bitcoin_get_balance"
 RECORD="(record { address = \"bcrt1qg4cvn305es3k8j69x06t9hf4v5yx4mxdaeazl8\"; network = variant { regtest }; min_confirmations = opt 10 })"
 EXPECTED="MinConfirmationsTooLarge"
-check_charging "${WALLET}" "${METHOD}" "${RECORD}" "${EXPECTED}"
+check_charging "${METHOD}" "${RECORD}" "${EXPECTED}"
 
+#test bitcoin_get_utxos
 METHOD="bitcoin_get_utxos"
 RECORD="(record { address = \"Bad address\"; network = variant { regtest } })"
 EXPECTED="MalformedAddress"
-check_charging "${WALLET}" "${METHOD}" "${RECORD}" "${EXPECTED}"
+check_charging "${METHOD}" "${RECORD}" "${EXPECTED}"
 
-METHOD="bitcoin_get_utxos"
 RECORD="(record { address = \"bcrt1qg4cvn305es3k8j69x06t9hf4v5yx4mxdaeazl8\"; network = variant { regtest }; filter = opt variant {min_confirmations = 10} })"
 EXPECTED="MinConfirmationsTooLarge"
-check_charging "${WALLET}" "${METHOD}" "${RECORD}" "${EXPECTED}"
+check_charging "${METHOD}" "${RECORD}" "${EXPECTED}"
 
-#METHOD="bitcoin_get_utxos"
 #RECORD="(record { address = \"bcrt1qg4cvn305es3k8j69x06t9hf4v5yx4mxdaeazl8\"; network = variant { regtest }; filter = opt variant {page = ${TX_BYTES}} })"
 #EXPECTED="MalformedPage"
-#check_charging "${WALLET}" "${METHOD}" "${RECORD}" "${EXPECTED}"
+#check_charging "${METHOD}" "${RECORD}" "${EXPECTED}"
 
 echo "SUCCESS"
