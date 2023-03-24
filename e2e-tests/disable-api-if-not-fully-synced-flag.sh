efab2518a9935c8bf3bc0e00dc915b3463024393
Dragoljub Djuric <dragoljub.duric@dfinity.org>
2023-03-24 13:24:08 +0100

    chore: Add e2e tests for disable_api_if_not_fully_synced (#158)

---
+++ e2e-tests/disable-api-if-not-fully-synced-flag.sh
@@ -0,0 +1,146 @@
+#!/usr/bin/env bash
+#
+# Verify that the Bitcoin canister respects the `disable_api_if_not_fully_synced` flag.
+set -Eexuo pipefail
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/utils.sh"
+
+# Run dfx stop if we run into errors.
+trap "dfx stop" EXIT SIGINT
+
+dfx start --background --clean
+
+# Deploy the canister that returns the blocks.
+dfx deploy --no-wallet e2e-disable-api-if-not-fully-synced-flag
+
+# Deploy the bitcoin canister, setting the blocks_source to be the source above.
+# And enabling 'disable_api_if_not_fully_synced'. 
+dfx deploy --no-wallet bitcoin --argument "(record {
+  stability_threshold = 1;
+  network = variant { regtest };
+  blocks_source = principal \"$(dfx canister id e2e-disable-api-if-not-fully-synced-flag)\";
+  syncing = variant { enabled };
+  fees = record {
+    get_utxos_base = 0;
+    get_utxos_cycles_per_ten_instructions = 0;
+    get_utxos_maximum = 0;
+    get_balance = 0;
+    get_balance_maximum = 0;
+    get_current_fee_percentiles = 0;
+    get_current_fee_percentiles_maximum = 0;
+    send_transaction_base = 0;
+    send_transaction_per_byte = 0;
+  };
+  api_access = variant { enabled };
+  disable_api_if_not_fully_synced = variant { enabled }
+})"
+
+# Wait until the ingestion of stable blocks is complete.
+# The number of next block headers should be 3, the canister
+# should reject all requests.
+wait_until_stable_height 2 60
+
+# bitcoin_get_balance should panic.
+set +e
+MSG=$(dfx canister call bitcoin bitcoin_get_balance '(record {
+  network = variant { regtest };
+  address = "bcrt1qg4cvn305es3k8j69x06t9hf4v5yx4mxdaeazl8"
+})' 2>&1);
+set -e
+
+if ! [[ $MSG = *"Canister state is not fully synced."* ]]; then
+  echo "FAIL"
+  exit 1
+fi
+
+# bitcoin_get_utxos should panic.
+set +e
+MSG=$(dfx canister call bitcoin bitcoin_get_utxos '(record {
+  network = variant { regtest };
+  address = "bcrt1qxp8ercrmfxlu0s543najcj6fe6267j97tv7rgf";
+})' 2>&1);
+set -e
+
+if ! [[ $MSG = *"Canister state is not fully synced."* ]]; then
+  echo "FAIL"
+  exit 1
+fi
+
+# bitcoin_get_current_fee_percentiles should panic.
+set +e
+MSG=$(dfx canister call bitcoin bitcoin_get_current_fee_percentiles '(record {
+  network = variant { regtest };
+})' 2>&1);
+set -e
+
+if ! [[ $MSG = *"Canister state is not fully synced."* ]]; then
+  echo "FAIL"
+  exit 1
+fi
+
+dfx stop
+
+dfx start --background --clean
+
+# Deploy the canister that returns the blocks.
+dfx deploy --no-wallet e2e-disable-api-if-not-fully-synced-flag
+
+# Deploy the bitcoin canister, setting the blocks_source to be the source above.
+# And disabling 'disable_api_if_not_fully_synced'. Hence, it should not make 
+# influence behaviour of the canister. 
+dfx deploy --no-wallet bitcoin --argument "(record {
+  stability_threshold = 1;
+  network = variant { regtest };
+  blocks_source = principal \"$(dfx canister id e2e-disable-api-if-not-fully-synced-flag)\";
+  syncing = variant { enabled };
+  fees = record {
+    get_utxos_base = 0;
+    get_utxos_cycles_per_ten_instructions = 0;
+    get_utxos_maximum = 0;
+    get_balance = 0;
+    get_balance_maximum = 0;
+    get_current_fee_percentiles = 0;
+    get_current_fee_percentiles_maximum = 0;
+    send_transaction_base = 0;
+    send_transaction_per_byte = 0;
+  };
+  api_access = variant { enabled };
+  disable_api_if_not_fully_synced = variant { disabled }
+})"
+
+# Wait until the ingestion of stable blocks is complete.
+wait_until_main_chain_height 2 60
+
+BALANCE=$(dfx canister call bitcoin bitcoin_get_balance '(record {
+  network = variant { regtest };
+  address = "bcrt1qg4cvn305es3k8j69x06t9hf4v5yx4mxdaeazl8"
+})')
+
+if ! [[ $BALANCE = "(2 : nat64)" ]]; then
+  echo "FAIL"
+  exit 1
+fi
+
+# Verify that we are able to fetch the UTXOs of one address.
+UTXOS=$(dfx canister call bitcoin bitcoin_get_utxos '(record {
+  network = variant { regtest };
+  address = "bcrt1qg4cvn305es3k8j69x06t9hf4v5yx4mxdaeazl8"
+})')
+
+# The address has 2 UTXOs.
+if ! [[ $(num_utxos "$UTXOS") = 2 ]]; then
+  echo "FAIL"
+  exit 1
+fi
+
+FEES=$(dfx canister call bitcoin bitcoin_get_current_fee_percentiles '(record {
+  network = variant { regtest };
+})')
+
+if ! [[ $FEES = "(vec {})" ]]; then
+  echo "FAIL"
+  exit 1
+fi
+
+echo "SUCCESS"
---
+++ e2e-tests/disable-api-if-not-fully-synced-flag/src/main.rs
@@ -0,0 +1,162 @@
+use bitcoin::{
+    blockdata::constants::genesis_block, consensus::Encodable, Address, Block, BlockHeader,
+    Network as BitcoinNetwork,
+};
+use candid::CandidType;
+use ic_btc_test_utils::{BlockBuilder, TransactionBuilder};
+use ic_cdk_macros::{init, update};
+use serde::{Deserialize, Serialize};
+use std::cell::{Cell, RefCell};
+use std::cmp::Ordering;
+use std::str::FromStr;
+
+type BlockBlob = Vec<u8>;
+type BlockHeaderBlob = Vec<u8>;
+type BlockHash = Vec<u8>;
+
+const ADDRESS: &str = "bcrt1qg4cvn305es3k8j69x06t9hf4v5yx4mxdaeazl8";
+
+// The number of blocks to generate (on top of genesis)
+const NUM_BLOCKS: usize = 2;
+
+const SYNCED_THRESHOLD: u32 = 2;
+
+#[derive(CandidType, Clone, Copy, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+enum Network {
+    #[serde(rename = "mainnet")]
+    Mainnet,
+    #[serde(rename = "testnet")]
+    Testnet,
+    #[serde(rename = "regtest")]
+    Regtest,
+}
+
+#[derive(CandidType, Clone, Debug, PartialEq, Eq, Deserialize)]
+enum GetSuccessorsRequest {
+    #[serde(rename = "initial")]
+    Initial(GetSuccessorsRequestInitial),
+    #[serde(rename = "follow_up")]
+    FollowUp(u8),
+}
+
+#[derive(CandidType, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+struct GetSuccessorsRequestInitial {
+    pub network: Network,
+    pub processed_block_hashes: Vec<BlockHash>,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, Hash, PartialEq, Eq, Serialize)]
+enum GetSuccessorsResponse {
+    #[serde(rename = "complete")]
+    Complete(GetSuccessorsCompleteResponse),
+    #[serde(rename = "partial")]
+    Partial(GetSuccessorsPartialResponse),
+    #[serde(rename = "follow_up")]
+    FollowUp(BlockBlob),
+}
+
+#[derive(CandidType, Clone, Debug, Default, Deserialize, Hash, PartialEq, Eq, Serialize)]
+struct GetSuccessorsCompleteResponse {
+    blocks: Vec<BlockBlob>,
+    next: Vec<BlockHeaderBlob>,
+}
+
+#[derive(CandidType, Clone, Debug, Default, Deserialize, Hash, PartialEq, Eq, Serialize)]
+struct GetSuccessorsPartialResponse {
+    partial_block: BlockBlob,
+    next: Vec<BlockHeaderBlob>,
+    remaining_follow_ups: u8,
+}
+
+thread_local! {
+    static BLOCKS: RefCell<Vec<BlockBlob>> = RefCell::new(Vec::new());
+
+    static COUNT: Cell<usize> = Cell::new(0);
+
+    static BLOCK_HEADERS: RefCell<Vec<BlockHeaderBlob>> = RefCell::new(Vec::new());
+}
+
+// Initialize the blocks.
+#[init]
+fn init() {
+    let network = BitcoinNetwork::Regtest;
+
+    // Generate NUM_BLOCKS blocks, each with NUM_TRANSACTIONS transactions.
+    let mut prev_header = genesis_block(network).header;
+    for _ in 0..NUM_BLOCKS {
+        let block = BlockBuilder::with_prev_header(prev_header)
+            .with_transaction(
+                TransactionBuilder::new()
+                    .with_output(&Address::from_str(ADDRESS).unwrap(), 1)
+                    .build(),
+            )
+            .build();
+        append_block(&block);
+        prev_header = block.header;
+    }
+
+    for _ in 0..SYNCED_THRESHOLD + 1 {
+        let next_block = BlockBuilder::with_prev_header(prev_header)
+            .with_transaction(
+                TransactionBuilder::new()
+                    .with_output(&Address::from_str(ADDRESS).unwrap(), 1)
+                    .build(),
+            )
+            .build();
+        append_block_header(&next_block.header);
+        prev_header = next_block.header;
+    }
+}
+
+#[update]
+fn bitcoin_get_successors(request: GetSuccessorsRequest) -> GetSuccessorsResponse {
+    if let GetSuccessorsRequest::Initial(GetSuccessorsRequestInitial { network, .. }) = &request {
+        assert_eq!(
+            *network,
+            Network::Regtest,
+            "request must be set to the regtest network"
+        );
+    }
+
+    let count = COUNT.with(|c| c.get());
+
+    let res = match Ord::cmp(&count, &(NUM_BLOCKS - 1)) {
+        // Send block in full.
+        Ordering::Less => GetSuccessorsResponse::Complete(GetSuccessorsCompleteResponse {
+            blocks: vec![BLOCKS.with(|b| b.borrow()[count].clone())],
+            next: vec![],
+        }),
+
+        Ordering::Equal => {
+            // Send block in full, and all next block headers.
+            GetSuccessorsResponse::Complete(GetSuccessorsCompleteResponse {
+                blocks: vec![BLOCKS.with(|b| b.borrow()[count].clone())],
+                next: BLOCK_HEADERS.with(|b| b.borrow().clone()),
+            })
+        }
+        Ordering::Greater => {
+            // Empty response
+            GetSuccessorsResponse::Complete(GetSuccessorsCompleteResponse {
+                blocks: vec![],
+                next: vec![],
+            })
+        }
+    };
+
+    COUNT.with(|c| c.set(c.get() + 1));
+    res
+}
+
+fn append_block(block: &Block) {
+    let mut block_bytes = vec![];
+    block.consensus_encode(&mut block_bytes).unwrap();
+    BLOCKS.with(|b| b.borrow_mut().push(block_bytes));
+}
+
+fn append_block_header(block_header: &BlockHeader) {
+    let mut block_bytes = vec![];
+    block_header.consensus_encode(&mut block_bytes).unwrap();
+    BLOCK_HEADERS.with(|b| b.borrow_mut().push(block_bytes));
+}
+
+fn main() {}
