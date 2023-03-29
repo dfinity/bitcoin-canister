056eb1dca1f90ce912399280e43264d21634f3cf
Islam El-Ashi <islam.elashi@dfinity.org>
2023-03-29 13:57:53 +0200

    feat: [EXC-1374] add script for running benchmarks. (#163)
    In a previous commit, a benchmarks canister was introduced to help
    benchmark specific parts of the code.
    
    This commit includes a script to run these benchmarks. The script relies
    on `drun`, which is configured to run benchmarks with practically no
    instruction limit. For now, it is assumed to be available in the
    caller's path.
    
    Running `./benchmarks/run.sh`, returns the following:
    
    ```
    (113_061_801_763 : nat64)
    ```
    
    In other words, the one benchmark we currently have takes 113B
    instructions to run (!!)

---
+++ benchmarks/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+pushd "$SCRIPT_DIR"
+
+# Build the benchmarks canister
+bash ../scripts/build-canister.sh benchmarks
+
+# Run the benchmarks, decode the output.
+drun ./drun.txt --instruction-limit 99999999999999 \
+    | awk '{ print $3 }' \
+    | grep "44.*" -o \
+    | xargs didc decode
