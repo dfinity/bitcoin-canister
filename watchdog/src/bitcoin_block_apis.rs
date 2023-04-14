7b6314ea2d1229f98c21a6286940e9ae60d0d336
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2023-04-14 17:02:51 +0200

    comments

--- watchdog/src/bitcoin_block_apis.rs
+++ watchdog/src/bitcoin_block_apis.rs
@@ -8,7 +8,7 @@ pub enum BitcoinBlockApi {
     ApiBitapsCom,
     ApiBlockchairCom,
     ApiBlockcypherCom,
-    BitcoinCanister, // Not an explorer.
+    BitcoinCanister, // Not an explorer. TODO: refactor, code smell.
     BlockchainInfo,
     BlockstreamInfo,
     ChainApiBtcCom,
