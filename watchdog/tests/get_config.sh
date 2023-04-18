93c01a5709464eb44e1af4e4efdbf68c023174b2
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2023-04-18 14:36:16 +0200

    add bash tests

---
+++ watchdog/tests/get_config.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# A test that verifies that the `get_config` endpoint works as expected.
+
+ITERATIONS=30
+DELAY_SEC=1
+
+# Run dfx stop if we run into errors.
+trap "dfx stop" EXIT SIGINT
+
+dfx start --background --clean
+
+# Deploy the watchdog canister.
+dfx deploy --no-wallet watchdog
+
+# Request config.
+config=$(dfx canister call watchdog get_config --query)
+
+# Check that the config is correct, eg. by checking the min_explores value.
+if ! [[ $config == *"min_explores = 3"* ]]; then
+  echo "FAIL"
+  exit 1
+fi
+
+echo "SUCCESS"
