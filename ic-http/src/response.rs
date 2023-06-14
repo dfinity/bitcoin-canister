6108d715ececdc9e893459e2058a0066da508258
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-06-14 08:55:12 +0000

    .

--- ic-http/src/response.rs
+++ ic-http/src/response.rs
@@ -8,7 +8,7 @@ pub fn create_response() -> HttpResponseBuilder {
 /// Represents a builder for an HTTP response.
 pub struct HttpResponseBuilder {
     /// The response status (e.g., 200, 404).
-    pub status: ic_cdk::export::candid::Nat,
+    pub status: candid::Nat,
     /// List of HTTP response headers and their corresponding values.
     pub headers: Vec<HttpHeader>,
     /// The response’s body.
@@ -18,7 +18,7 @@ pub struct HttpResponseBuilder {
 impl HttpResponseBuilder {
     pub fn new() -> Self {
         Self {
-            status: ic_cdk::export::candid::Nat::from(200),
+            status: candid::Nat::from(200),
             headers: Vec::new(),
             body: Vec::new(),
         }
