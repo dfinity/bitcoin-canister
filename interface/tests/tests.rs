49111b674e0c38a9994500aad4eac2a106da1391
Roman Kashitsyn <roman@dfinity.org>
2023-06-30 15:30:58 +0000

    add a proptest for bytebuf decoding

--- interface/tests/tests.rs
+++ interface/tests/tests.rs
@@ -1,6 +1,8 @@
 use ic_btc_interface::Txid;
 use proptest::array::uniform32;
+use proptest::collection::vec as pvec;
 use proptest::prelude::*;
+use serde_bytes::ByteBuf;
 use std::str::FromStr;
 
 proptest! {
@@ -11,10 +13,19 @@ proptest! {
     }
 
     #[test]
-    fn serde_cbor_decode(bytes in uniform32(any::<u8>())) {
+    fn serde_cbor_decode_byte_array(bytes in uniform32(any::<u8>())) {
         let mut encoded = vec![];
         ciborium::into_writer(&bytes, &mut encoded).unwrap();
         let txid: Txid = ciborium::from_reader(&encoded[..]).unwrap();
         prop_assert_eq!(txid, Txid::from(bytes));
+    }
+
+    #[test]
+    fn serde_cbor_decode_bytebuf(bytes in pvec(any::<u8>(), 32)) {
+        let mut encoded = vec![];
+        let original_txid = Txid::try_from(&bytes[..]).unwrap();
+        ciborium::into_writer(&ByteBuf::from(bytes), &mut encoded).unwrap();
+        let txid: Txid = ciborium::from_reader(&encoded[..]).unwrap();
+        prop_assert_eq!(txid, original_txid);
     }
 }
