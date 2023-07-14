541978425c05feecaa7911a03311e013cc7dbc98
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2023-07-14 16:25:06 +0000

    cleanup

--- ic-http/src/http_request.rs
+++ ic-http/src/http_request.rs
@@ -6,18 +6,6 @@ use ic_cdk::api::management_canister::http_request::{CanisterHttpRequestArgument
 /// Errors on the IC have two components; a Code and a message associated with it.
 pub type CallResult<R> = Result<R, (RejectionCode, String)>;
 
-// /// Make a HTTP request to a given URL and return HTTP response, possibly after a transformation.
-// #[cfg(not(target_arch = "wasm32"))]
-// pub async fn http_request(arg: CanisterHttpRequestArgument) -> CallResult<(HttpResponse,)> {
-//     crate::mock::http_request(arg).await
-// }
-
-// /// Make an HTTP request to a given URL and return the HTTP response, possibly after a transformation.
-// #[cfg(target_arch = "wasm32")]
-// pub async fn http_request(arg: CanisterHttpRequestArgument) -> CallResult<(HttpResponse,)> {
-//     ic_cdk::api::management_canister::http_request::http_request(arg).await
-// }
-
 /// Make a HTTP request to a given URL and return HTTP response, possibly after a transformation.
 #[cfg(not(target_arch = "wasm32"))]
 pub async fn http_request(
