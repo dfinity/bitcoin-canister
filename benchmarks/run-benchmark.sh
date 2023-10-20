6a6bc8fc43b51de59e76929d5e5c74d9c19cf216
Dragoljub Duric <dragoljub.duric@dfinity.org>
2023-10-20 10:04:12 +0000

    refactor based on comments

--- benchmarks/run-benchmark.sh
+++ benchmarks/run-benchmark.sh
@@ -3,12 +3,47 @@
 # Runs a benchmark using drun. The script assumes drun is available on the caller's path.
 set -euo pipefail
 
+# Remove downloaded didc, drun.
+trap 'set +e && rm drun didc && set -e' EXIT SIGINT
+
 BENCH_NAME=$1
 FILE=$(mktemp)
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Add script directory to the PATH
+export PATH="$SCRIPT_DIR:$PATH"
+
+get_didc_release(){
+  OS=$(uname)
+  ARCH=$(uname -m)
+  if [ "$OS" == "Darwin" ] && [ "$ARCH" == "arm64" ]; then
+    # Apple sillicon
+    wget -O didc https://github.com/dfinity/candid/releases/download/2023-07-25/didc-macos
+  elif [ "$OS" == "Linux" ] && [ "$ARCH" == "x86_64" ]; then
+          # Linux x86 64bit
+          wget -O didc https://github.com/dfinity/candid/releases/download/2023-07-25/didc-linux64
+      else
+          echo "Unsoported machine"
+          EXIT SIGINT
+  fi
+}
+
+get_drun_release() {
+  OS=$(uname | tr '[:upper:]' '[:lower:]')
+  URL="https://github.com/dfinity/ic/releases/download/release-2023-09-27_23-01%2Bquic/drun-x86_64-${OS}.gz"
+  wget -O "drun.gz" "${URL}"
+  gzip -d drun.gz
+}
+
+if ! type "didc" > /dev/null; then
+  get_didc_release
+  chmod +x didc
+fi
+
 if ! type "drun" > /dev/null; then
-  echo "drun is not installed. Please add drun to your path from commit d35535c96184be039aaa31f68b48bbe45909494e."
-  exit 1
+  get_drun_release 
+  chmod +x drun
 fi
 
 cat > "$FILE" << EOF
--- e2e-tests/benchmark.sh
+++ e2e-tests/benchmark.sh
@@ -1,43 +1,5 @@
 #!/usr/bin/env bash
 set -Eexuo pipefail
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-# Add root directory to the PATH
-export PATH="$SCRIPT_DIR/..:$PATH"
-
-ROOT_DIR="$SCRIPT_DIR/.."
-
-cd "$ROOT_DIR"
-
-# Remove downloaded didc, drun.
-trap 'rm $ROOT_DIR/didc $ROOT_DIR/drun' EXIT SIGINT
-
-get_didc_release(){
-  OS=$(uname)
-  ARCH=$(uname -m)
-  if [ "$OS" == "Darwin" ] && [ "$ARCH" == "arm64" ]; then
-    # Apple sillicon
-    wget -O didc https://github.com/dfinity/candid/releases/download/2023-07-25/didc-macos
-  elif [ "$OS" == "Linux" ] && [ "$ARCH" == "x86_64" ]; then
-          # Linux x86 64bit
-          wget -O didc https://github.com/dfinity/candid/releases/download/2023-07-25/didc-linux64
-      else
-          echo "Unsoported machine"
-          EXIT SIGINT
-  fi
-}
-
-get_drun_release() {
-  OS=$(uname | tr '[:upper:]' '[:lower:]')
-  URL="https://github.com/dfinity/ic/releases/download/release-2023-09-27_23-01%2Bquic/drun-x86_64-${OS}.gz"
-  wget -O "drun.gz" "${URL}"
-  gzip -d drun.gz
-}
-
-get_didc_release
-chmod +x didc
-
-get_drun_release 
-chmod +x drun
 
 set +e
 REGRESSIONS=$(cargo bench | grep -c "regressed by")
