1a422c3b1402bfabaf8b388ae0658039121f8fe3
Islam El-Ashi <islam.elashi@dfinity.org>
2024-07-18 10:47:51 -0400

    move main state builder to state-builder cargo

--- bootstrap/6_compute_canister_state.sh
+++ bootstrap/6_compute_canister_state.sh
@@ -38,8 +38,7 @@ cargo run --release --bin combine-state -- \
    --output $CANISTER_STATE_FILE --canister-state-dir $CANISTER_STATE_DIR
 
 echo "Building state struct.."
-pushd main-state-builder
-cargo run --release -- \
+cargo run --release --features file_memory -- \
    --canister-state "../$CANISTER_STATE_FILE" \
    --canister-state-dir "../$CANISTER_STATE_DIR" \
    --network "$NETWORK" \
