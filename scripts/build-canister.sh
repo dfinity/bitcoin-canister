3059c73c19ae95859696042537acd343f7aa3a62
Andriy Berestovskyy <andriy.berestovskyy@dfinity.org>
2024-10-04 14:27:31 +0200

    chore: Update gitignore and minor fixes

--- scripts/build-canister.sh
+++ scripts/build-canister.sh
@@ -41,15 +41,15 @@ if [[ "$STATUS" -eq "0" ]]; then
 
     if [[ "$CANISTER" == "ic-btc-canister" ]]; then
       ./target/bin/ic-wasm \
-      "./target/$TARGET/release/$CANISTER.wasm" \
-      -o "./target/$TARGET/release/$CANISTER.wasm" \
+      "./target/$TARGET/${PROFILE}/$CANISTER.wasm" \
+      -o "./target/$TARGET/${PROFILE}/$CANISTER.wasm" \
       metadata candid:service -f "$SCRIPT_DIR/../canister/candid.did" -v public
     fi
 
     if [[ "$CANISTER" == "watchdog" ]]; then
       ./target/bin/ic-wasm \
-      "./target/$TARGET/release/$CANISTER.wasm" \
-      -o "./target/$TARGET/release/$CANISTER.wasm" \
+      "./target/$TARGET/${PROFILE}/$CANISTER.wasm" \
+      -o "./target/$TARGET/${PROFILE}/$CANISTER.wasm" \
       metadata candid:service -f "$SCRIPT_DIR/../watchdog/candid.did" -v public
     fi
 
