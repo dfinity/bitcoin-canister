41c5a7a231291c7c214de8aac3c1b048c9f8d8af
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-11-25 14:57:25 +0000

    add generate_config

--- bootstrap/1_download_state.sh
+++ bootstrap/1_download_state.sh
@@ -22,23 +22,8 @@ mkdir data
 
 # Create a bitcoin.conf file that downloads blocks up to the given height.
 CONF_FILE=$(mktemp)
-cat << EOF > "$CONF_FILE"
 # Stop running after reaching the given height in the main chain.
-stopatheight=$HEIGHT
-
-# Reduce storage requirements by only storing most recent N MiB of block.
-prune=5000
-
-# Dummy credentials that are required by bitcoin-cli.
-rpcuser=ic-btc-integration
-rpcpassword=QPQiNaph19FqUsCrBRN0FII7lyM26B51fAMeBQzCb-E=
-rpcauth=ic-btc-integration:cdf2741387f3a12438f69092f0fdad8e\$62081498c98bee09a0dce2b30671123fa561932992ce377585e8e08bb0c11dfa
-EOF
-
-# Configure bitcoin.conf to connect to the testnet network if needed.
-if [[ "$NETWORK" == "testnet" ]]; then
-    echo "chain=test" >> "$CONF_FILE"
-fi
+generate_config "$NETWORK" "$CONF_FILE" "stopatheight=$HEIGHT"
 
 LOG_FILE=$(mktemp)
 echo "Downloading the bitcoin blocks up to height $HEIGHT (output streamed to $LOG_FILE)
--- bootstrap/utils.sh
+++ bootstrap/utils.sh
@@ -12,3 +12,33 @@ validate_network() {
         exit 1
     fi
 }
+
+# Generate the Bitcoin configuration file with optional parameters.
+generate_config() {
+    local network=$1
+    local conf_file=$2
+    shift 2
+    local additional_params=("$@")  # Collect additional flags.
+
+    # Basic configuration.
+    cat << EOF > "$conf_file"
+# Reduce storage requirements by only storing the most recent N MiB of blocks.
+prune=5000
+
+# Dummy credentials required by bitcoin-cli.
+rpcuser=ic-btc-integration
+rpcpassword=QPQiNaph19FqUsCrBRN0FII7lyM26B51fAMeBQzCb-E=
+rpcauth=ic-btc-integration:cdf2741387f3a12438f69092f0fdad8e\$62081498c98bee09a0dce2b30671123fa561932992ce377585e8e08bb0c11dfa
+EOF
+
+    # Add network-specific settings.
+    case "$network" in
+        "mainnet") echo "# Mainnet settings" >> "$conf_file" ;;
+        "testnet") echo "chain=test" >> "$conf_file" ;;
+    esac
+
+    # Add additional parameters.
+    for param in "${additional_params[@]}"; do
+        echo "$param" >> "$conf_file"
+    done
+}
