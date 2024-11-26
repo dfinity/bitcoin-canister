9d2da87ccb57ede4150794ec4272772dd31cd3dc
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-11-26 10:06:02 +0000

    fix typo

--- bootstrap/4_compute_utxo_dump.sh
+++ bootstrap/4_compute_utxo_dump.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+#
 # A script for building the UTXO dump text file.
 set -euo pipefail
 
@@ -25,7 +27,20 @@ tail -n +2 "$UTXO_DUMP" > "$UTXO_DUMP.tmp" && mv "$UTXO_DUMP.tmp" "$UTXO_DUMP"
 
 echo "Sorting the file..."
 
-	@@ -40,7 +46,7 @@ export LC_MEASUREMENT="C.UTF-8"
+# Set the locale to make `sort -n` deterministic.
+export LANG=C.UTF-8
+export LANGUAGE=
+export LC_CTYPE=C.UTF-8
+export LC_NUMERIC="C.UTF-8"
+export LC_TIME="C.UTF-8"
+export LC_COLLATE="C.UTF-8"
+export LC_MONETARY="C.UTF-8"
+export LC_MESSAGES="C.UTF-8"
+export LC_PAPER="C.UTF-8"
+export LC_NAME="C.UTF-8"
+export LC_ADDRESS="C.UTF-8"
+export LC_TELEPHONE="C.UTF-8"
+export LC_MEASUREMENT="C.UTF-8"
 export LC_IDENTIFICATION="C.UTF-8"
 export LC_ALL=
 
