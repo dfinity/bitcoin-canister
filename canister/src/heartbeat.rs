a4400c86300798ed96fe003b1439d95169ecc9c8
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-01-21 16:06:56 +0000

    testnet chain max depth drop from 100k to 500

--- canister/src/heartbeat.rs
+++ canister/src/heartbeat.rs
@@ -182,8 +182,7 @@ async fn maybe_fetch_blocks() -> bool {
                 ));
             }
         },
-        Err((code, msg)) => {
-            print(&format!("Error fetching blocks: [{:?}] {}", code, msg));
+        Err(_) => {
         }
     }
 
