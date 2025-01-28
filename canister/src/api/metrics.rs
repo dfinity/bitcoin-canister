52679b7d915d695b86b087b93f1fa4044ea4477d
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-01-28 16:09:29 +0000

    turn off metrics

--- canister/src/api/metrics.rs
+++ canister/src/api/metrics.rs
@@ -169,20 +169,20 @@ fn encode_metrics(w: &mut MetricsEncoder<Vec<u8>>) -> std::io::Result<()> {
         .value(&[("flag", "enabled")], enabled)?
         .value(&[("flag", "disabled")], disabled)?;
 
-        if let Some(stats) = &state.syncing_state.successor_response_stats {
-            encode_labeled_gauge(
-                w,
-                "successor_response_block_count",
-                "Block count statistics for the latest GetSuccessorsResponse.",
-                &stats.get_block_count_metrics(),
-            )?;
-            encode_labeled_gauge(
-                w,
-                "successor_response_block_size",
-                "Block size statistics for the latest GetSuccessorsResponse.",
-                &stats.get_block_size_metrics(),
-            )?;
-        }
+        // if let Some(stats) = &state.syncing_state.successor_response_stats {
+        //     encode_labeled_gauge(
+        //         w,
+        //         "successor_response_block_count",
+        //         "Block count statistics for the latest GetSuccessorsResponse.",
+        //         &stats.get_block_count_metrics(),
+        //     )?;
+        //     encode_labeled_gauge(
+        //         w,
+        //         "successor_response_block_size",
+        //         "Block size statistics for the latest GetSuccessorsResponse.",
+        //         &stats.get_block_size_metrics(),
+        //     )?;
+        // }
 
         Ok(())
     })
