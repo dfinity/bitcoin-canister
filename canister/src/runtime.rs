dfb80ab6bf8100153c19d5bd2853365a30b8bf15
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-02-05 16:42:28 +0000

    time_secs

--- canister/src/runtime.rs
+++ canister/src/runtime.rs
@@ -201,21 +201,8 @@ pub fn get_cycles_balance() -> u64 {
     CYCLES_BALANCE.with(|c| *c.borrow())
 }
 
-/// Returns the current time in seconds.
-#[cfg(target_arch = "wasm32")]
-pub fn time() -> u64 {
-    // to get seconds from nanoseconds
-    ic_cdk::api::time() / 1_000_000_000
-}
-
-#[cfg(not(target_arch = "wasm32"))]
-pub fn time() -> u64 {
-    use std::time::SystemTime;
-
-    SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_secs()
+pub fn time_secs() -> u64 {
+    time_nanos() / 1_000_000_000
 }
 
 /// Returns the current time in seconds.
