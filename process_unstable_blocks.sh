57aa59d076de4b595bb1aeaa63d05294a97b146e
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-03-26 12:06:35 +0000

    .

--- process_unstable_blocks.sh
+++ process_unstable_blocks.sh
@@ -16,5 +16,5 @@ dfx canister call --network testnet bitcoin_t get_unstable_blocks > "${BASE}.txt
 echo "Parsing ${BASE}.txt to ${BASE}.json ..."
 ./unstable_blocks.py "${BASE}.txt" "${BASE}.json"
 
-echo "Generating blockchain graph to blockchain_${SUFFIX}.png ..."
-./visualize_blockchain_graphviz.py "${BASE}.json" "blockchain_${SUFFIX}.png"
+echo "Generating blockchain graph to ${BASE}.png ..."
+./visualize_blockchain_graphviz.py "${BASE}.json" "${BASE}.png"
