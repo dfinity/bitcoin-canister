f2c73ac04bb04312e220062eab65e99629ac9771
Mathieu Ducroux <mathieu.ducroux@dfinity.org>
2025-06-17 11:36:41 +0200

    fix(validation): add timestamp validation check testnet4

--- validation/src/header.rs
+++ validation/src/header.rs
@@ -69,11 +69,7 @@ pub fn validate_header(
         }
     };
 
-    if *network != Network::Testnet4 {
-        // Skip timestamp validation for Testnet4; the first 2 blocks are 2 days apart.
-        // https://mempool.space/testnet4/block/00000000da84f2bafbbc53dee25a72ae507ff4914b867c565be350b0da8bf043
-        is_timestamp_valid(store, header, current_time)?;
-    }
+    is_timestamp_valid(store, header, current_time)?;
 
     let header_target = header.target();
     if header_target > max_target(network) {
