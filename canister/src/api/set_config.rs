b2be127698c3429d842914680f7366c0ddc0b011
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-06-27 13:32:45 +0200

    rm reset sync state in set_config

--- canister/src/api/set_config.rs
+++ canister/src/api/set_config.rs
@@ -35,9 +35,6 @@ fn set_api_access(request: SetConfigRequest) {
 pub(crate) fn set_config_no_verification(request: SetConfigRequest) {
     crate::with_state_mut(|s| {
         if let Some(syncing) = request.syncing {
-            if syncing == Flag::Disabled {
-                reset_syncing_state(s);
-            }
             s.syncing_state.syncing = syncing;
         }
 
