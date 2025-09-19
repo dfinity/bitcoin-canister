2ba82a2321e1b43b5e5c82750052cb8291674df6
gregorydemay <gregory.demay@dfinity.org>
2025-09-19 10:01:54 +0200

    XC-497: move test setup

--- validation/src/header/tests.rs
+++ validation/src/header/tests.rs
@@ -14,112 +14,6 @@ use crate::constants::test::{
 
 const MOCK_CURRENT_TIME: u64 = 2_634_590_600;
 
-#[derive(Clone)]
-struct StoredHeader {
-    header: Header,
-    height: BlockHeight,
-}
-
-struct SimpleHeaderStore {
-    headers: HashMap<BlockHash, StoredHeader>,
-    height: BlockHeight,
-    tip_hash: BlockHash,
-    initial_hash: BlockHash,
-}
-
-impl SimpleHeaderStore {
-    fn new(initial_header: Header, height: BlockHeight) -> Self {
-        let initial_hash = initial_header.block_hash();
-        let tip_hash = initial_header.block_hash();
-        let mut headers = HashMap::new();
-        headers.insert(
-            initial_hash,
-            StoredHeader {
-                header: initial_header,
-                height,
-            },
-        );
-
-        Self {
-            headers,
-            height,
-            tip_hash,
-            initial_hash,
-        }
-    }
-
-    fn add(&mut self, header: Header) {
-        let prev = self
-            .headers
-            .get(&header.prev_blockhash)
-            .expect("prev hash missing");
-        let stored_header = StoredHeader {
-            header,
-            height: prev.height + 1,
-        };
-
-        self.height = stored_header.height;
-        self.headers.insert(header.block_hash(), stored_header);
-        self.tip_hash = header.block_hash();
-    }
-}
-
-impl HeaderStore for SimpleHeaderStore {
-    fn get_with_block_hash(&self, hash: &BlockHash) -> Option<Header> {
-        self.headers.get(hash).map(|stored| stored.header)
-    }
-
-    fn get_with_height(&self, height: u32) -> Option<Header> {
-        let blocks_to_traverse = self.height - height;
-        let mut header = self.headers.get(&self.tip_hash).unwrap().header;
-        for _ in 0..blocks_to_traverse {
-            header = self.headers.get(&header.prev_blockhash).unwrap().header;
-        }
-        Some(header)
-    }
-
-    fn height(&self) -> u32 {
-        self.height
-    }
-
-    fn get_initial_hash(&self) -> BlockHash {
-        self.initial_hash
-    }
-}
-
-fn deserialize_header(encoded_bytes: &str) -> Header {
-    let bytes = Vec::from_hex(encoded_bytes).expect("failed to decoded bytes");
-    deserialize(bytes.as_slice()).expect("failed to deserialize")
-}
-
-/// This function reads `num_headers` headers from `tests/data/headers.csv`
-/// and returns them.
-fn get_bitcoin_headers() -> Vec<Header> {
-    let rdr = Reader::from_path(
-        PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("tests/data/headers.csv"),
-    );
-    assert!(rdr.is_ok(), "Unable to find blockchain_headers.csv file");
-    let mut rdr = rdr.unwrap();
-    let mut headers = vec![];
-    for result in rdr.records() {
-        let record = result.unwrap();
-        let header = Header {
-            version: Version::from_consensus(
-                i32::from_str_radix(record.get(0).unwrap(), 16).unwrap(),
-            ),
-            prev_blockhash: BlockHash::from_str(record.get(1).unwrap()).unwrap(),
-            merkle_root: TxMerkleNode::from_str(record.get(2).unwrap()).unwrap(),
-            time: u32::from_str_radix(record.get(3).unwrap(), 16).unwrap(),
-            bits: CompactTarget::from_consensus(
-                u32::from_str_radix(record.get(4).unwrap(), 16).unwrap(),
-            ),
-            nonce: u32::from_str_radix(record.get(5).unwrap(), 16).unwrap(),
-        };
-        headers.push(header);
-    }
-    headers
-}
-
 #[test]
 fn test_simple_mainnet() {
     let header_705600 = deserialize_header(MAINNET_HEADER_705600);
@@ -461,4 +355,110 @@ fn test_compute_next_difficulty_for_backdated_blocks() {
 
     // Assert.
     assert_eq!(difficulty, CompactTarget::from_consensus(473956288));
+}
+
+#[derive(Clone)]
+struct StoredHeader {
+    header: Header,
+    height: BlockHeight,
+}
+
+struct SimpleHeaderStore {
+    headers: HashMap<BlockHash, StoredHeader>,
+    height: BlockHeight,
+    tip_hash: BlockHash,
+    initial_hash: BlockHash,
+}
+
+impl SimpleHeaderStore {
+    fn new(initial_header: Header, height: BlockHeight) -> Self {
+        let initial_hash = initial_header.block_hash();
+        let tip_hash = initial_header.block_hash();
+        let mut headers = HashMap::new();
+        headers.insert(
+            initial_hash,
+            StoredHeader {
+                header: initial_header,
+                height,
+            },
+        );
+
+        Self {
+            headers,
+            height,
+            tip_hash,
+            initial_hash,
+        }
+    }
+
+    fn add(&mut self, header: Header) {
+        let prev = self
+            .headers
+            .get(&header.prev_blockhash)
+            .expect("prev hash missing");
+        let stored_header = StoredHeader {
+            header,
+            height: prev.height + 1,
+        };
+
+        self.height = stored_header.height;
+        self.headers.insert(header.block_hash(), stored_header);
+        self.tip_hash = header.block_hash();
+    }
+}
+
+impl HeaderStore for SimpleHeaderStore {
+    fn get_with_block_hash(&self, hash: &BlockHash) -> Option<Header> {
+        self.headers.get(hash).map(|stored| stored.header)
+    }
+
+    fn get_with_height(&self, height: u32) -> Option<Header> {
+        let blocks_to_traverse = self.height - height;
+        let mut header = self.headers.get(&self.tip_hash).unwrap().header;
+        for _ in 0..blocks_to_traverse {
+            header = self.headers.get(&header.prev_blockhash).unwrap().header;
+        }
+        Some(header)
+    }
+
+    fn height(&self) -> u32 {
+        self.height
+    }
+
+    fn get_initial_hash(&self) -> BlockHash {
+        self.initial_hash
+    }
+}
+
+fn deserialize_header(encoded_bytes: &str) -> Header {
+    let bytes = Vec::from_hex(encoded_bytes).expect("failed to decoded bytes");
+    deserialize(bytes.as_slice()).expect("failed to deserialize")
+}
+
+/// This function reads `num_headers` headers from `tests/data/headers.csv`
+/// and returns them.
+fn get_bitcoin_headers() -> Vec<Header> {
+    let rdr = Reader::from_path(
+        PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("tests/data/headers.csv"),
+    );
+    assert!(rdr.is_ok(), "Unable to find blockchain_headers.csv file");
+    let mut rdr = rdr.unwrap();
+    let mut headers = vec![];
+    for result in rdr.records() {
+        let record = result.unwrap();
+        let header = Header {
+            version: Version::from_consensus(
+                i32::from_str_radix(record.get(0).unwrap(), 16).unwrap(),
+            ),
+            prev_blockhash: BlockHash::from_str(record.get(1).unwrap()).unwrap(),
+            merkle_root: TxMerkleNode::from_str(record.get(2).unwrap()).unwrap(),
+            time: u32::from_str_radix(record.get(3).unwrap(), 16).unwrap(),
+            bits: CompactTarget::from_consensus(
+                u32::from_str_radix(record.get(4).unwrap(), 16).unwrap(),
+            ),
+            nonce: u32::from_str_radix(record.get(5).unwrap(), 16).unwrap(),
+        };
+        headers.push(header);
+    }
+    headers
 }
