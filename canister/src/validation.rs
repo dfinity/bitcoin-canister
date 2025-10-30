90766bd1ce14a4ab9b7b48536f683f373494ee7a
Paul H. Liu <paul@dfinity.org>
2025-10-30 07:16:49 -0700

    Revert "Optimize ValidationContext"
    This reverts commit 95e9a6394cb4d23597bd897c4a49d713ac04c0cc.

--- canister/src/validation.rs
+++ canister/src/validation.rs
@@ -1,15 +1,14 @@
-use crate::{blocktree::BlockChain, state::State, unstable_blocks};
+use crate::{state::State, unstable_blocks};
 use bitcoin::{block::Header, hashes::Hash};
-use ic_btc_types::{Block, BlockHash};
+use ic_btc_types::BlockHash;
 use ic_btc_validation::HeaderStore;
 
 /// A structure passed to the validation crate to validate a specific block header.
 pub struct ValidationContext<'a> {
     state: &'a State,
-    unstable_blocks_chain: BlockChain<'a, Block>,
     // BlockHash is stored in order to avoid repeatedly calling to
     // Header::block_hash() which is expensive.
-    headers_chain: Vec<(&'a Header, ic_btc_types::BlockHash)>,
+    chain: Vec<(&'a Header, ic_btc_types::BlockHash)>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -36,12 +35,13 @@ impl<'a> ValidationContext<'a> {
         {
             return Err(ValidationContextError::AlreadyKnown(current_block_hash));
         }
+        let chain = chain
+            .into_chain()
+            .iter()
+            .map(|block| (block.header(), block.block_hash().clone()))
+            .collect();
 
-        Ok(Self {
-            state,
-            unstable_blocks_chain: chain,
-            headers_chain: vec![],
-        })
+        Ok(Self { state, chain })
     }
 
     /// Initialize a `ValidationContext` for the given block header.
@@ -58,23 +58,11 @@ impl<'a> ValidationContext<'a> {
             Self::new(state, header)
         } else {
             let mut context = Self::new(state, next_block_headers_chain[0].0)?;
-            context.headers_chain = next_block_headers_chain;
+            for item in next_block_headers_chain.iter() {
+                context.chain.push(item.clone())
+            }
             Ok(context)
         }
-    }
-
-    #[cfg(test)]
-    fn chain(self) -> Vec<(&'a Header, BlockHash)> {
-        self.unstable_blocks_chain
-            .into_chain()
-            .iter()
-            .map(|block| (block.header(), block.block_hash().clone()))
-            .chain(
-                self.headers_chain
-                    .into_iter()
-                    .map(|(header, hash)| (header, hash.clone())),
-            )
-            .collect::<Vec<_>>()
     }
 }
 
@@ -83,33 +71,20 @@ impl HeaderStore for ValidationContext<'_> {
     fn get_with_block_hash(&self, hash: &bitcoin::BlockHash) -> Option<Header> {
         // Check if the header is in the chain.
         let hash = ic_btc_types::BlockHash::from(hash.as_raw_hash().as_byte_array().to_vec());
-        self.headers_chain
-            .iter()
-            .find_map(|(&header, block_hash)| {
-                if block_hash == &hash {
-                    Some(header)
-                } else {
-                    None
-                }
-            })
-            .or_else(|| {
-                self.unstable_blocks_chain
-                    .find(&hash)
-                    .map(|block| *block.header())
-            })
-            .or_else(|| {
-                // The header is in the stable store.
-                self.state.stable_block_headers.get_with_block_hash(&hash)
-            })
+        for item in self.chain.iter() {
+            if item.1 == hash {
+                return Some(*item.0);
+            }
+        }
+
+        // The header is in the stable store.
+        self.state.stable_block_headers.get_with_block_hash(&hash)
     }
 
     fn height(&self) -> u32 {
         // The `next_height` method returns the height of the UTXOs + 1, so we
         // subtract 1 to account for that.
-        self.state.utxos.next_height()
-            + self.unstable_blocks_chain.len() as u32
-            + self.headers_chain.len() as u32
-            - 1
+        self.state.utxos.next_height() + self.chain.len() as u32 - 1
     }
 
     fn get_with_height(&self, height: u32) -> Option<Header> {
@@ -120,15 +95,7 @@ impl HeaderStore for ValidationContext<'_> {
         } else if height <= self.height() {
             // The height requested is for an unstable block.
             // Retrieve the block header from the chain.
-            let mut index = (height - self.state.utxos.next_height()) as usize;
-            if index < self.unstable_blocks_chain.len() {
-                self.unstable_blocks_chain
-                    .get(index)
-                    .map(|block| *block.header())
-            } else {
-                index -= self.unstable_blocks_chain.len();
-                self.headers_chain.get(index).map(|(&header, _)| header)
-            }
+            Some(*self.chain[(height - self.state.utxos.next_height()) as usize].0)
         } else {
             // The height requested is higher than the tip.
             None
@@ -175,7 +142,7 @@ mod test {
             ValidationContext::new_with_next_block_headers(&state, block_3.header()).unwrap();
 
         assert_eq!(
-            validation_context.chain(),
+            validation_context.chain,
             vec![
                 (genesis.header(), genesis.block_hash().clone()),
                 (block_0.header(), block_0.block_hash().clone()),
