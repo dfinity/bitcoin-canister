dfinity/dogecoin-canister

feat: cache unstable blocks in stable memory (#54)
In order to avoid taking up too much heap space, it is desirable to use
stable memory as a cache to store unstable blocks. When blocks are
removed from the unstable blocks structure, they are also removed from
the cache.

A new CachedBlock struct is introduced to replace the Block type at most
places. It keeps the block hash while leaving the actual block in cache.
It also keeps a copy of pre-calculated difficulty because the difficulty
is frequently needed to calculate the longest chain.

the state types are also made generic for the purpose of writing tests
to ensure upgrading from old state to new state can succeed. This
modification can be reverted once we upgrade all deployed canisters.

---------

Co-authored-by: gregorydemay <112856886+gregorydemay@users.noreply.github.com>

canister/src/api/get_utxos.rs
@@ -1,12 +1,12 @@
 use crate::{
-    blocktree::BlockChain,
+    blocktree::{BlockChain, CachedBlock, ChainBlock},
     charge_cycles,
     runtime::{performance_counter, print},
     types::{Address, GetUtxosRequest, Page, Utxo},
     unstable_blocks, verify_has_enough_cycles, with_state, with_state_mut, State,
 };
 use ic_doge_interface::{GetUtxosError, GetUtxosResponse, Utxo as PublicUtxo, UtxosFilter};
-use ic_doge_types::{Block, BlockHash, OutPoint, Txid};
+use ic_doge_types::{BlockHash, OutPoint, Txid};
 use serde_bytes::ByteBuf;
 use std::str::FromStr;
 
@@ -175,16 +175,16 @@ fn get_utxos_internal(
 //    stability_count(b) = d(b) if |D(b)| = 0 and d(b) - max_{b' ∈ D(b)} d(b') otherwise.
 // ```
 fn get_stability_count(
-    blocks_with_depths_on_the_same_height: &[(&Block, u32)],
+    blocks_with_depths_on_the_same_height: &[(&BlockHash, u32)],
     target_block: &BlockHash,
 ) -> i32 {
     let mut max_depth_of_the_other_blocks = 0;
     let mut target_block_depth = 0;
-    for (block, depth) in blocks_with_depths_on_the_same_height.iter() {
-        if block.block_hash() != target_block {
-            max_depth_of_the_other_blocks = std::cmp::max(max_depth_of_the_other_blocks, *depth);
+    for &(block_hash, depth) in blocks_with_depths_on_the_same_height.iter() {
+        if block_hash != target_block {
+            max_depth_of_the_other_blocks = std::cmp::max(max_depth_of_the_other_blocks, depth);
         } else {
-            target_block_depth = *depth;
+            target_block_depth = depth;
         }
     }
     target_block_depth as i32 - max_depth_of_the_other_blocks as i32
@@ -194,7 +194,7 @@ fn get_utxos_from_chain(
     state: &State,
     address: &str,
     min_confirmations: u32,
-    chain: BlockChain<Block>,
+    chain: BlockChain<CachedBlock>,
     offset: Option<Utxo>,
     utxo_limit: usize,
 ) -> Result<(GetUtxosResponse, Stats), GetUtxosError> {
@@ -214,7 +214,7 @@ fn get_utxos_from_chain(
     let mut tip_block_hash = chain.first().block_hash();
     let mut tip_block_height = state.utxos.next_height();
 
-    let blocks_with_depths_by_heights = state.unstable_blocks.blocks_with_depths_by_heights();
+    let blocks_with_depths_by_heights = state.unstable_blocks.block_hashes_with_depths_by_heights();
 
     // Apply unstable blocks to the UTXO set.
     let ins_start = performance_counter();
@@ -228,7 +228,7 @@ fn get_utxos_from_chain(
         }
         tip_block_hash = block.block_hash();
         tip_block_height = state.utxos.next_height() + (i as u32);
-        address_utxos.apply_block(block);
+        address_utxos.apply_block(&block.block());
     }
     stats.ins_apply_unstable_blocks = performance_counter() - ins_start;
 
@@ -287,7 +287,7 @@ mod test {
     use super::*;
     use crate::{
         genesis_block, runtime, state,
-        test_utils::{BlockBuilder, BlockChainBuilder, TransactionBuilder},
+        test_utils::{BlockBuilder, BlockChainBuilder, TestBlocksCache, TransactionBuilder},
         types::into_dogecoin_network,
         with_state_mut,
     };
@@ -981,7 +981,8 @@ mod test {
             .with_transaction(tx)
             .build();
 
-        let mut state = State::new(2, network, block_0);
+        let cache = TestBlocksCache::new(network);
+        let mut state = State::new(cache, 2, network, block_0);
         state::insert_block(&mut state, block_1.clone()).unwrap();
 
         // Address 1 should have no UTXOs at zero confirmations.
@@ -1026,7 +1027,8 @@ mod test {
                 block_builder = block_builder.with_transaction(transaction.clone());
             }
             let block_0 = block_builder.build();
-            let state = State::new(2, network, block_0.clone());
+            let cache = TestBlocksCache::new(network);
+            let state = State::new(cache, 2, network, block_0.clone());
             let tip_block_hash = block_0.block_hash();
 
             let utxo_set = get_utxos_internal(
@@ -1137,7 +1139,8 @@ mod test {
                 prev_block = Some(block);
             }
 
-            let mut state = State::new(2, network, blocks[0].clone());
+        let cache = TestBlocksCache::new(network);
+            let mut state = State::new(cache, 2, network, blocks[0].clone());
             for block in blocks[1..].iter() {
                 state::insert_block(&mut state, block.clone()).unwrap();
             }
@@ -1247,7 +1250,7 @@ mod test {
     #[test]
     fn test_get_stability_count_single_block_on_height() {
         let block = BlockBuilder::genesis().build();
-        let blocks_with_depths: Vec<(&Block, u32)> = vec![(&block, 1)];
+        let blocks_with_depths: Vec<(&BlockHash, u32)> = vec![(block.block_hash(), 1)];
         // Stability count should be 1.
         assert_eq!(
             get_stability_count(&blocks_with_depths, block.block_hash()),
@@ -1261,7 +1264,11 @@ mod test {
         let block2 = BlockBuilder::genesis().build();
         let block3 = BlockBuilder::genesis().build();
 
-        let blocks_with_depths: Vec<(&Block, u32)> = vec![(&block1, 5), (&block2, 7), (&block3, 3)];
+        let blocks_with_depths: Vec<(&BlockHash, u32)> = vec![
+            (block1.block_hash(), 5),
+            (block2.block_hash(), 7),
+            (block3.block_hash(), 3),
+        ];
         // The stability_count of block1 should be 5 - 7 = -2.
         assert_eq!(
             get_stability_count(&blocks_with_depths, block1.block_hash()),

canister/src/blocktree/serde.rs
@@ -1,11 +1,15 @@
-use super::{Block, BlockTree};
+use super::{Block, BlockHash, BlockTree, BlocksCache, CachedBlock};
+use bitcoin::block::Header;
 use bitcoin::dogecoin::Block as DogecoinBlock;
+use ic_doge_interface::Network;
 use serde::{
     de::{Deserializer, Error, SeqAccess, Visitor},
     ser::SerializeSeq,
     Deserialize, Serialize, Serializer,
 };
+use std::cell::RefCell;
 use std::fmt;
+use std::rc::Rc;
 
 /// Serialization helper to flatten a nested tree into a list.
 #[derive(Default)]
@@ -39,6 +43,29 @@ impl Serialize for BlockTree<Block> {
             #[cfg(feature = "canbench-rs")]
             let _p = canbench_rs::bench_scope("serialize_blocktree_flatten");
             flattened.map_from(self, &|block| block.internal_bitcoin_block());
+        }
+        {
+            #[cfg(feature = "canbench-rs")]
+            let _p = canbench_rs::bench_scope("serialize_blocktree_serialize_seq");
+            flattened.serialize(serializer)
+        }
+    }
+}
+
+/// Serialize `BlockTree<CachedBlock>` by flattening it into a list of
+/// `(Header, BlockHash, Difficulty)` pairs.
+///
+/// Note that the actual block is not serialized here because they are
+/// stored separately in a BlocksCache.
+impl Serialize for BlockTree<CachedBlock> {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut flattened = FlattenedTree(vec![]);
+        {
+            #[cfg(feature = "canbench-rs")]
+            let _p = canbench_rs::bench_scope("serialize_blocktree_flatten");
+            flattened.map_from(self, &|block| {
+                (&block.header, &block.block_hash, &block.difficulty)
+            });
         }
         {
             #[cfg(feature = "canbench-rs")]
@@ -119,5 +146,80 @@ impl<'de> Deserialize<'de> for BlockTree<Block> {
         D: Deserializer<'de>,
     {
         deserializer.deserialize_seq(BlockTreeVisitor)
+    }
+}
+
+/// Visitor for `BlockTree<CachedBlock>`
+struct CachedBlockTreeVisitor(Rc<RefCell<Box<dyn BlocksCache>>>);
+
+impl<'de> TreeVisitor<'de, CachedBlock> for CachedBlockTreeVisitor {
+    fn next<A: SeqAccess<'de>>(&self, seq: &mut A) -> Result<(CachedBlock, usize), A::Error> {
+        seq.next_element::<((Header, BlockHash, u128), usize)>()?
+            .map(|((header, block_hash, difficulty), size)| {
+                let block = CachedBlock {
+                    cache: self.0.clone(),
+                    block_hash,
+                    difficulty,
+                    header,
+                };
+                (block, size)
+            })
+            .ok_or(A::Error::custom("reading next element must succeed"))
+    }
+}
+
+impl<'de> Visitor<'de> for CachedBlockTreeVisitor {
+    type Value = BlockTree<CachedBlock>;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("A blocktree deserializer.")
+    }
+
+    fn visit_seq<A: SeqAccess<'de>>(self, seq: A) -> Result<Self::Value, A::Error> {
+        self.visit_sequence(seq)
+    }
+}
+
+/// The deserialization of a BlockTree<CachedBlock> would construct all CachedBlock
+/// using a dummy cache. This is a hack because it is difficult to pass an existing
+/// cache into the deserialization process, especially when the BlockTree is
+/// serveral levels down in a nested struct that derives Deserialize. The caller
+/// is expected to manually replace the dummy cache with the real cache after
+/// deserialization, otherwise any call into the dummy cache will panic.
+impl<'de> Deserialize<'de> for BlockTree<CachedBlock> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let cache: Rc<RefCell<Box<dyn BlocksCache>>> = Rc::new(RefCell::new(Box::new(DummyCache)));
+        deserializer.deserialize_seq(CachedBlockTreeVisitor(cache))
+    }
+}
+
+/// Dummy implementation of BlocksCache that always panics.
+#[derive(Debug)]
+struct DummyCache;
+
+impl BlocksCache for DummyCache {
+    fn insert(&mut self, _block_hash: BlockHash, _block: Block) -> bool {
+        unimplemented!()
+    }
+    fn remove(&mut self, _block_hash: &BlockHash) -> bool {
+        unimplemented!()
+    }
+    fn get(&self, _block_hash: &BlockHash) -> Option<Block> {
+        unimplemented!()
+    }
+    fn is_empty(&self) -> bool {
+        unimplemented!()
+    }
+    fn len(&self) -> u64 {
+        unimplemented!()
+    }
+    fn network(&self) -> Network {
+        unimplemented!()
+    }
+    fn collect(&self) -> std::collections::BTreeMap<BlockHash, Block> {
+        unimplemented!()
     }
 }

dfinity/bitcoin-canister

canister/src/api/get_utxos.rs
@@ -1,12 +1,12 @@
 use crate::{
-    blocktree::BlockChain,
+    blocktree::{BlockChain, CachedBlock, ChainBlock},
     charge_cycles,
     runtime::{performance_counter, print},
     types::{Address, AddressParseError, GetUtxosRequest, Page, Utxo},
     unstable_blocks, verify_has_enough_cycles, with_state, with_state_mut, State,
 };
 use ic_btc_interface::{GetUtxosError, GetUtxosResponse, Utxo as PublicUtxo, UtxosFilter};
-use ic_btc_types::{Block, BlockHash, OutPoint, Txid};
+use ic_btc_types::{BlockHash, OutPoint, Txid};
 use serde_bytes::ByteBuf;
 
 // The maximum number of UTXOs that are allowed to be included in a single
@@ -174,16 +174,16 @@ fn get_utxos_internal(
 //    stability_count(b) = d(b) if |D(b)| = 0 and d(b) - max_{b' ∈ D(b)} d(b') otherwise.
 // ```
 fn get_stability_count(
-    blocks_with_depths_on_the_same_height: &[(&Block, u32)],
+    blocks_with_depths_on_the_same_height: &[(&BlockHash, u32)],
     target_block: &BlockHash,
 ) -> i32 {
     let mut max_depth_of_the_other_blocks = 0;
     let mut target_block_depth = 0;
-    for (block, depth) in blocks_with_depths_on_the_same_height.iter() {
-        if block.block_hash() != target_block {
-            max_depth_of_the_other_blocks = std::cmp::max(max_depth_of_the_other_blocks, *depth);
+    for &(block_hash, depth) in blocks_with_depths_on_the_same_height.iter() {
+        if block_hash != target_block {
+            max_depth_of_the_other_blocks = std::cmp::max(max_depth_of_the_other_blocks, depth);
         } else {
-            target_block_depth = *depth;
+            target_block_depth = depth;
         }
     }
     target_block_depth as i32 - max_depth_of_the_other_blocks as i32
@@ -193,7 +193,7 @@ fn get_utxos_from_chain(
     state: &State,
     address: &str,
     min_confirmations: u32,
-    chain: BlockChain<Block>,
+    chain: BlockChain<CachedBlock>,
     offset: Option<Utxo>,
     utxo_limit: usize,
 ) -> Result<(GetUtxosResponse, Stats), GetUtxosError> {
@@ -218,7 +218,7 @@ fn get_utxos_from_chain(
     let mut tip_block_hash = chain.first().block_hash();
     let mut tip_block_height = state.utxos.next_height();
 
-    let blocks_with_depths_by_heights = state.unstable_blocks.blocks_with_depths_by_heights();
+    let blocks_with_depths_by_heights = state.unstable_blocks.block_hashes_with_depths_by_heights();
 
     // Apply unstable blocks to the UTXO set.
     let ins_start = performance_counter();
@@ -232,7 +232,7 @@ fn get_utxos_from_chain(
         }
         tip_block_hash = block.block_hash();
         tip_block_height = state.utxos.next_height() + (i as u32);
-        address_utxos.apply_block(block);
+        address_utxos.apply_block(&block.block());
     }
     stats.ins_apply_unstable_blocks = performance_counter() - ins_start;
 
@@ -291,7 +291,7 @@ mod test {
     use super::*;
     use crate::{
         genesis_block, runtime, state,
-        test_utils::{BlockBuilder, BlockChainBuilder, TransactionBuilder},
+        test_utils::{BlockBuilder, BlockChainBuilder, TestBlocksCache, TransactionBuilder},
         types::into_bitcoin_network,
         with_state_mut,
     };
@@ -1098,7 +1098,8 @@ mod test {
             .with_transaction(tx)
             .build();
 
-        let mut state = State::new(2, network, block_0);
+        let cache = TestBlocksCache::new(network);
+        let mut state = State::new(cache, 2, network, block_0);
         state::insert_block(&mut state, block_1.clone()).unwrap();
 
         // Address 1 should have no UTXOs at zero confirmations.
@@ -1143,7 +1144,8 @@ mod test {
                 block_builder = block_builder.with_transaction(transaction.clone());
             }
             let block_0 = block_builder.build();
-            let state = State::new(2, network, block_0.clone());
+            let cache = TestBlocksCache::new(network);
+            let state = State::new(cache, 2, network, block_0.clone());
             let tip_block_hash = block_0.block_hash();
 
             let utxo_set = get_utxos_internal(
@@ -1254,7 +1256,8 @@ mod test {
                 prev_block = Some(block);
             }
 
-            let mut state = State::new(2, network, blocks[0].clone());
+        let cache = TestBlocksCache::new(network);
+            let mut state = State::new(cache, 2, network, blocks[0].clone());
             for block in blocks[1..].iter() {
                 state::insert_block(&mut state, block.clone()).unwrap();
             }
@@ -1364,7 +1367,7 @@ mod test {
     #[test]
     fn test_get_stability_count_single_block_on_height() {
         let block = BlockBuilder::genesis().build();
-        let blocks_with_depths: Vec<(&Block, u32)> = vec![(&block, 1)];
+        let blocks_with_depths: Vec<(&BlockHash, u32)> = vec![(block.block_hash(), 1)];
         // Stability count should be 1.
         assert_eq!(
             get_stability_count(&blocks_with_depths, block.block_hash()),
@@ -1378,7 +1381,11 @@ mod test {
         let block2 = BlockBuilder::genesis().build();
         let block3 = BlockBuilder::genesis().build();
 
-        let blocks_with_depths: Vec<(&Block, u32)> = vec![(&block1, 5), (&block2, 7), (&block3, 3)];
+        let blocks_with_depths: Vec<(&BlockHash, u32)> = vec![
+            (block1.block_hash(), 5),
+            (block2.block_hash(), 7),
+            (block3.block_hash(), 3),
+        ];
         // The stability_count of block1 should be 5 - 7 = -2.
         assert_eq!(
             get_stability_count(&blocks_with_depths, block1.block_hash()),

canister/src/blocktree/serde.rs
@@ -1,10 +1,14 @@
-use super::{Block, BlockTree};
+use super::{Block, BlockHash, BlockTree, BlocksCache, CachedBlock};
+use bitcoin::block::Header;
+use ic_btc_interface::Network;
 use serde::{
     de::{Deserializer, Error, SeqAccess, Visitor},
     ser::SerializeSeq,
     Deserialize, Serialize, Serializer,
 };
+use std::cell::RefCell;
 use std::fmt;
+use std::rc::Rc;
 
 /// Serialization helper to flatten a nested tree into a list.
 #[derive(Default)]
@@ -38,6 +42,29 @@ impl Serialize for BlockTree<Block> {
             #[cfg(feature = "canbench-rs")]
             let _p = canbench_rs::bench_scope("serialize_blocktree_flatten");
             flattened.map_from(self, &|block| block.internal_bitcoin_block());
+        }
+        {
+            #[cfg(feature = "canbench-rs")]
+            let _p = canbench_rs::bench_scope("serialize_blocktree_serialize_seq");
+            flattened.serialize(serializer)
+        }
+    }
+}
+
+/// Serialize `BlockTree<CachedBlock>` by flattening it into a list of
+/// `(Header, BlockHash, Difficulty)` pairs.
+///
+/// Note that the actual block is not serialized here because they are
+/// stored separately in a BlocksCache.
+impl Serialize for BlockTree<CachedBlock> {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut flattened = FlattenedTree(vec![]);
+        {
+            #[cfg(feature = "canbench-rs")]
+            let _p = canbench_rs::bench_scope("serialize_blocktree_flatten");
+            flattened.map_from(self, &|block| {
+                (&block.header, &block.block_hash, &block.difficulty)
+            });
         }
         {
             #[cfg(feature = "canbench-rs")]
@@ -118,5 +145,80 @@ impl<'de> Deserialize<'de> for BlockTree<Block> {
         D: Deserializer<'de>,
     {
         deserializer.deserialize_seq(BlockTreeVisitor)
+    }
+}
+
+/// Visitor for `BlockTree<CachedBlock>`
+struct CachedBlockTreeVisitor(Rc<RefCell<Box<dyn BlocksCache>>>);
+
+impl<'de> TreeVisitor<'de, CachedBlock> for CachedBlockTreeVisitor {
+    fn next<A: SeqAccess<'de>>(&self, seq: &mut A) -> Result<(CachedBlock, usize), A::Error> {
+        seq.next_element::<((Header, BlockHash, u128), usize)>()?
+            .map(|((header, block_hash, difficulty), size)| {
+                let block = CachedBlock {
+                    cache: self.0.clone(),
+                    block_hash,
+                    difficulty,
+                    header,
+                };
+                (block, size)
+            })
+            .ok_or(A::Error::custom("reading next element must succeed"))
+    }
+}
+
+impl<'de> Visitor<'de> for CachedBlockTreeVisitor {
+    type Value = BlockTree<CachedBlock>;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("A blocktree deserializer.")
+    }
+
+    fn visit_seq<A: SeqAccess<'de>>(self, seq: A) -> Result<Self::Value, A::Error> {
+        self.visit_sequence(seq)
+    }
+}
+
+/// The deserialization of a BlockTree<CachedBlock> would construct all CachedBlock
+/// using a dummy cache. This is a hack because it is difficult to pass an existing
+/// cache into the deserialization process, especially when the BlockTree is
+/// serveral levels down in a nested struct that derives Deserialize. The caller
+/// is expected to manually replace the dummy cache with the real cache after
+/// deserialization, otherwise any call into the dummy cache will panic.
+impl<'de> Deserialize<'de> for BlockTree<CachedBlock> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let cache: Rc<RefCell<Box<dyn BlocksCache>>> = Rc::new(RefCell::new(Box::new(DummyCache)));
+        deserializer.deserialize_seq(CachedBlockTreeVisitor(cache))
+    }
+}
+
+/// Dummy implementation of BlocksCache that always panics.
+#[derive(Debug)]
+struct DummyCache;
+
+impl BlocksCache for DummyCache {
+    fn insert(&mut self, _block_hash: BlockHash, _block: Block) -> bool {
+        unimplemented!()
+    }
+    fn remove(&mut self, _block_hash: &BlockHash) -> bool {
+        unimplemented!()
+    }
+    fn get(&self, _block_hash: &BlockHash) -> Option<Block> {
+        unimplemented!()
+    }
+    fn is_empty(&self) -> bool {
+        unimplemented!()
+    }
+    fn len(&self) -> u64 {
+        unimplemented!()
+    }
+    fn network(&self) -> Network {
+        unimplemented!()
+    }
+    fn collect(&self) -> std::collections::BTreeMap<BlockHash, Block> {
+        unimplemented!()
     }
 }